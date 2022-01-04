276da0cd0020ec2628279c260054faa529dde0fa
newmersedez <trishkk2001@gmail.com>
2022-01-04 16:48:02 +0300

    server sends json to client

--- source/server.py
+++ source/server.py
@@ -17,6 +17,7 @@ class Server:
         while True:
             s.listen()
             conn, addr = s.accept()
+            json_string = ''
             with conn:
                 print('Connected by', addr)
                 while True:
@@ -24,86 +25,108 @@ class Server:
                         data = conn.recv(1024)
                         if not data:
                             break
-                        if data == b'Lessons':
-                            self._dataBaseLessonsRequest()
-                        elif data == b'Exams':
-                            self._dataBaseExamsRequest()
-                        elif data == b'Marks':
-                            self._dataBaseMarksRequest()
-                        elif data == b'Events':
-                            self._dataBaseEventsRequest()
+                        print('RECV = {}'.format(data))
+                        if data == b'Lessons' or data == b'Lessons\r\n':
+                            json_string = self._dataBaseLessonsRequest()
+                        elif data == b'Exams' or data == b'Exams\r\n':
+                            json_string = self._dataBaseExamsRequest()
+                        elif data == b'Marks' or data == b'Marks\r\n':
+                            json_string = self._dataBaseMarksRequest()
+                        elif data == b'Events' or data == b'Events\r\n':
+                            json_string = self._dataBaseEventsRequest()
+                        print('SEND = {}'.format(json_string.encode()))
+                        conn.send(json_string.encode())
                     except ConnectionError:
                         break
 
     @staticmethod
     def _dataBaseLessonsRequest():
         conn = None
+        json_text = ""
         try:
             params = config()
             conn = psycopg2.connect(**params)
             cur = conn.cursor()
-            cur.execute(open('sql_scripts/get_lessons.sql').read().format('Понедельник'))
-            db = cur.fetchall()
-            cur.close()
+            columns = ('lesson_day', 'lesson_time', 'lesson_type', 'classroom_name', 'subject_name')
+            cur.execute(open('sql_scripts/get_lessons.sql').read())
+            results = []
 
+            for row in cur.fetchall():
+                results.append(dict(zip(columns, row)))
+            json_text = json.dumps(results, ensure_ascii=False, default=str).encode('utf8').decode()
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
             if conn is not None:
                 conn.close()
-                [print(item) for item in db]
+        return json_text
 
     @staticmethod
     def _dataBaseExamsRequest():
         conn = None
+        json_text = ""
         try:
             params = config()
             conn = psycopg2.connect(**params)
             cur = conn.cursor()
+            columns = ('subject_name', 'group_name', 'exam_date', 'exam_time', 'classroom_name')
             cur.execute(open('sql_scripts/get_exams.sql').read())
-            db = cur.fetchall()
-            cur.close()
+            results = []
 
+            for row in cur.fetchall():
+                results.append(dict(zip(columns, row)))
+            json_text = json.dumps(results, ensure_ascii=False, default=str).encode('utf8').decode()
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
             if conn is not None:
                 conn.close()
-                [print(item) for item in db]
+        return json_text
 
     @staticmethod
     def _dataBaseMarksRequest():
         conn = None
+        json_text = ""
         try:
             params = config()
             conn = psycopg2.connect(**params)
             cur = conn.cursor()
+            columns = ('student_lastname', 'student_name', 'student_patronymic', 'subject_name', 'mark')
             cur.execute(open('sql_scripts/get_marks.sql').read().format('Тришин', 'Дмитрий', 'Александрович'))
-            db = cur.fetchall()
-            cur.close()
+            results = []
 
+            for row in cur.fetchall():
+                results.append(dict(zip(columns, row)))
+            json_text = json.dumps(results, ensure_ascii=False, default=str).encode('utf8').decode()
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
             if conn is not None:
                 conn.close()
-                print(type(db))
-                [print(item) for item in db]
+        return json_text
 
     @staticmethod
     def _dataBaseEventsRequest():
         conn = None
+        json_text = ""
         try:
             params = config()
             conn = psycopg2.connect(**params)
             cur = conn.cursor()
+            columns = ('event_name', 'event_date', 'event_time', 'classroom_name', 'group_name')
             cur.execute(open('sql_scripts/get_events.sql').read())
-            db = cur.fetchall()
-            cur.close()
+            results = []
 
+            for row in cur.fetchall():
+                results.append(dict(zip(columns, row)))
+            json_text = json.dumps(results, ensure_ascii=False, default=str).encode('utf8').decode()
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
             if conn is not None:
                 conn.close()
-                [print(item) for item in db]
+        return json_text
--- sql_scripts/get_lessons.sql
+++ sql_scripts/get_lessons.sql
@@ -1,5 +1,5 @@
-select lt.lesson_day, lt.lesson_time, lt.lesson_type, s.subject_name
+select lt.lesson_day, lt.lesson_time, lt.lesson_type, c.classroom_name, s.subject_name
 from lesson_timetable as lt
 left join subject s on lt.subject_id = s.subject_id
-where lt.lesson_day = '{}'
+left join classroom c on lt.classroom_id = c.classroom_id
 order by lesson_id
